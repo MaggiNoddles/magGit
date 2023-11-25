2829bc3cb85ca254450848f6cdebc8aafcf81e2a
maggien <mn888consulting@gmail.com>
2023-11-25 16:50:07 +1100

    testbranch

---
+++ src/testbranch.ts
@@ -0,0 +1,19 @@
+import {question} from 'readline-sync';
+
+//example to test
+function fillUniqueArray() {
+    const nums: number [] = new Array<number>(10);
+
+    for (let i = 0; i < nums.length; i++) {
+        const numText = question('Enter a number: ');
+        console.log(numText);
+    }
+}
+
+fillUniqueArray();
+
+//has this been stagged? if not. 
+
+//okay this is working. 
+
+//test branch time take 2.
